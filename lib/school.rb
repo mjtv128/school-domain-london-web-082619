fd18d767fd01813c30d1597082cd3d46883206df
jocular-source-7570 <mjtvan@gmail.com>
2019-08-27 23:33:12 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
 # code here!
+class School 
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+  
+  def roster 
+    @roster 
+  end 
+
+def add_student(student_name, grade)
+  if @roster[grade] != nil
+    @roster[grade] << student_name
+  else
+  @roster[grade] = [student_name]
+end 
+end 
+
+def grade(grade)
+  @roster[grade]
+end 
+
+def sort 
+  @roster.each{|grade, student|
+  @roster[grade] = student.sort
+  }
+end 
+
+
+
+end 
